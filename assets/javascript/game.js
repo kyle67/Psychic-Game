4e3877e9465ea5211aa897dcf1fde75b6097da23
Kyle Daniels <kyledaniels@Kyles-MacBook-Air.local>
2019-09-11 23:15:09 -0400

    Modified game.js with array containing alphabet, defined variables for game, and created the function event wwith local and global variables.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,51 @@
-<script>
-document.addEventListener('keydown', logKey);
+// Create an arrary with the letters from a to z
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-function logKey(e) {
-  log.textContent += ` ${e.code}`;
+// Defines variables with settings for zero and number of guesses
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letterUser = [];
+var eachofLetters = null;
 
-  document.querySelector(“#wins”).innerHTML = wins;
+
+// Sets the computerGuess variable equal to a random choice from the computerChoice array.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 }
-        </script>
+
+function farUserGuesses() {
+	document.querySelector("#letter").innerHTML = "Your Guesses so far: " + letterUser.join(' ');
+}
+
+countGuessesLeft();
+
+var restart = function() {
+	guessesLeft = 9;
+	letterUser = [];
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+// When the player enters a key the function event will execute
+document.onkeyup = function(event) {
+	guessesLeft--;
+
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	letterUser.push(userGuess);
+	countGuessesLeft();
+	farUserGuesses();
+
+	if (userGuess === computerGuess){
+		wins++;
+		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		restart();
+	} 
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#lose").innerHTML = "Loses: " + losses;
+		restart();
+	}
+  };
